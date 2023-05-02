005fe0d48f9afc1257e13420fb96f984c2709ff4
YUYUN043025 <78917925+YUYUN043025@users.noreply.github.com>
2023-05-02 21:39:41 +0800

    Add files via upload

---
+++ 8times.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+from PIL import Image
+
+images_path = '/userhome/cs2/yyxyy/nerf/GP_data/nerf_llff_data/Channel1/images/' # 原图路径
+output_dir = '/userhome/cs2/yyxyy/nerf/GP_data/nerf_llff_data/Channel1/images_8/' # resize后路径
+
+factor = 8 # 降采样倍数
+
+images_list = os.listdir(images_path)
+img = Image.open(images_path + images_list[0])
+(W,H) = (img.width,img.height) #[W,H]
+print("image_size : ",(W ,H))
+
+for image_name in images_list:
+    if image_name == '.DS_Store':
+        pass
+    else:
+        img = cv2.imread(images_path+image_name)
+        img_resize = cv2.resize(img, (int(W/factor), int(H/factor)))
+        cv2.imwrite(output_dir + image_name, img_resize)
+        print(image_name , " done")
+print("all images done")
